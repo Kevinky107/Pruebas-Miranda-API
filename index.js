5015f8ff88310141c3adefd989c2606bcae6e11a
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-08-01 12:09:35 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const formJSON = {}
+
+    for(const [key, value] of formData.entries()) {
+        formJSON[key] = value;
+    }
+
+    fetch("https://olqdivqcuh.execute-api.eu-west-3.amazonaws.com/dev/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formJSON)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Success:", data);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+})
