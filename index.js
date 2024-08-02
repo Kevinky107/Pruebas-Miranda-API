96c371d0e296a53f63bd5b52e5baca7945683880
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-08-02 11:29:48 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("form");
+const btn = document.getElementById("rooms");
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -10,7 +11,27 @@ form.addEventListener("submit", function(event) {
         formJSON[key] = value;
     }
 
-    fetch("https://olqdivqcuh.execute-api.eu-west-3.amazonaws.com/dev/login", {
+    fetch("https://ufuj3gm3b3.execute-api.eu-west-3.amazonaws.com/dev/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formJSON)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Success:", data);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+})
+
+btn.addEventListener("click", function(event) {
+    event.preventDefault();
+
+
+    fetch("https://ufuj3gm3b3.execute-api.eu-west-3.amazonaws.com/dev/rooms", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
